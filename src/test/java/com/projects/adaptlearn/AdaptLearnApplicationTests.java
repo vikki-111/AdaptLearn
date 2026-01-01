ebc22f163b671a32fcad1591fc535d8abbb03100
Vikas <saivikas2006@gmail.com>
2026-01-01 19:42:38 +0530

    Initial commit

---
+++ src/test/java/com/projects/adaptlearn/AdaptLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.adaptlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
